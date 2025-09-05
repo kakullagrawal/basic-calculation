608298231903e25ab51ad08516f98484ac577f26
Jashandeep Singh <jashandeepsinghofficial1@gmail.com>
2025-09-05 13:23:11 +0530

    Update basic calculation kakul.c
    Made it more optimized.

--- basic calculation kakul.c
+++ basic calculation kakul.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int a = 10;
-	int b = 20;
-	
-	printf("%d\n", a + b);
-	printf("%d\n", a - b);
-	printf("%d\n", a * b);
-	printf("%d\n", b / a);
+	int a = 10,b = 20;
+	printf("Sum is = %d\n", a + b);
+	printf("Subtraction is = %d\n", a - b);
+	printf("Product is = %d\n", a * b);
+	printf("Division is = %d\n", b / a);
 	
 	return 0;
 	
 }
+
